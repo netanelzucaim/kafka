60520857b64dfdd3c37ba711c042f091f784d30b
root <root@ST25-144.cs.colman.ac.il>
2025-05-28 20:09:48 +0300

    changed woodpecker

--- consumer.py
+++ consumer.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 
 consumer = KafkaConsumer(
     'test-topic',
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers='kafka-service:9092',
     auto_offset_reset='earliest',
     group_id='my-consumer-group',
     enable_auto_commit=True
